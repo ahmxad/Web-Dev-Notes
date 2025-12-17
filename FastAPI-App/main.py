192d88544afc2c209804d41268d852a4ca5a9f80
ahmxad <drevex88@gmail.com>
2025-12-17 13:10:33 +0500

    fun

--- FastAPI-App/main.py
+++ FastAPI-App/main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to the Randomizer API"}
+DATABASE_URL = "postgresql://postgres:abc12345@localhost:5432/demo_db"
+engine = create_engine(DATABASE_URL)
+
+# -------- Schema (request body) --------
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+
+# -------- GET: sab users laao --------
+@app.get("/users")
+def get_users():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT id, email FROM users")) # you can also retrive password 😹
+        rows = result.fetchall()
+
+    return [dict(row._mapping) for row in rows]
+
+
+# -------- POST: naya user banao --------
+@app.post("/users")
+def create_user(user: UserCreate):
+    with engine.connect() as conn:
+        conn.execute(
+            text("""
+                INSERT INTO users (email, password)
+                VALUES (:email, :password)
+            """),
+            {
+                "email": user.email,
+                "password": user.password
+            }
+        )
+        conn.commit()
+
+    return {"status": "user created"}
