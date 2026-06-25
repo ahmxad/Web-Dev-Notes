1ea983515ef19069beffd9d72bdf7409447a6e5e
ahmxad <drevex88@gmail.com>
2026-06-25 15:08:35 +0500

    crap

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -2,17 +2,10 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-// EJS
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "/views"))
-// PORT
 let PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")))
+
 app.listen(PORT, ()=>{
     console.log(`Running at ${PORT} 🐥`)
-})
-// HOME
-app.get("/ig/:username", (req, res)=>{
-    let {username} = req.params;
-    let val = Math.floor(Math.random()*6)+1;
-    res.render("home", {username, val})
 })
